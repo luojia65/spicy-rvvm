43eab8fb9122a4021e98c10f02b8d508f8526735
luojia65 <me@luojia.cc>
2019-09-03 12:15:59 +0800

    Read input ELF file

--- spicy-rvvm/src/main.rs
+++ spicy-rvvm/src/main.rs
@@ -1,3 +1,21 @@
+use clap::*;
+use std::fs::OpenOptions;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("spicy-rvvm")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("elf-input-path")
+            .value_name("PATH")
+            .takes_value(true)
+            .required(true)
+            .help("path to the input RISC-V elf file"))
+        .get_matches();
+    let input_path_str = matches.value_of("elf-input-path").unwrap();
+    // println!("{}", input_path_str);
+    let file = OpenOptions::new().read(true).open(input_path_str)
+        .expect("open input file");
+    // todo
+    let _ = file;
 }
